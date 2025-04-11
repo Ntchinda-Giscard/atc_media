c7a9a4073c0c41e7f0f6c6e75d9539fc17a9d520
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-04-11 17:09:26 +0100

    add docker file and docker ignore file

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         sans: ["var(--font-sans)", ...fontFamily.sans],
+//       },
+//     },
+//   },
+  plugins: [
+    require("tailwindcss-animate"), // 👈 this is all you need to add
+  ],
+};
