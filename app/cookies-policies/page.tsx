afae534c0e368cac1f96b2c6d02b8796cfda268a
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-04-14 15:53:34 +0100

    cookies page

--- app/cookies-policies/page.tsx
+++ app/cookies-policies/page.tsx
@@ -5,10 +5,33 @@ function Page() {
                 <div className="flex flex-col text-center w-full">
                     <h1 className="text-3xl font-semibold"> 🍪 Politique Relative aux Cookies </h1>
                     <p className="font-light mt-4 text-sm px-10 mb-20"> Bienvenue sur [Nom du site]. La protection de vos données personnelles est une priorité pour nous. Cette politique explique quelles informations nous collectons, comment nous les utilisons et comment vous pouvez exercer vos droits.</p>
-
-                    <p className="text-sm mb-5"> Bienvenue sur <b> ATC media </b>. Cette politique explique comment et pourquoi nous utilisons des cookies et technologies similaires sur notre plateforme. </p>
-                    <p className="text-sm mb-5"> 📌 En continuant à naviguer sur notre site, vous acceptez l’utilisation des cookies conformément à cette politique. </p>
                 </div>
+                <div className="flex flex-col gap-3 px-7">
+                    <p className="text-sm mb-2"> Bienvenue sur <b> ATC media </b>. Cette politique explique comment et pourquoi nous utilisons des cookies et technologies similaires sur notre plateforme. </p>
+                    <p className="text-sm mb-3"> 📌 En continuant à naviguer sur notre site, vous acceptez l’utilisation des cookies conformément à cette politique. </p>
+
+                    <p>Dans le cadre de l’exploitation du site <b> tv.atcmedia.fr </b>, des cookies peuvent être déposés sur le terminal de l’utilisateur (ordinateur, tablette, smartphone), notamment lors de la première connexion. Ces cookies ont pour but d’améliorer votre expérience utilisateur, de garantir le bon fonctionnement technique du site et de recueillir des données statistiques de navigation dans le respect de la législation en vigueur, notamment le RGPD et la directive ePrivacy.</p>
+
+                    <h1 className="text-2xl font-bold"> Définition et utilité des cookies </h1>
+                    <p> Un cookie est un petit fichier texte enregistré sur votre appareil lorsque vous consultez un site internet. Il permet notamment de mémoriser des préférences, de faciliter votre navigation ou encore de mesurer la fréquentation des différentes pages, afin d’améliorer en continu les services proposés. </p>
+
+                    <h1 className="text-2xl font-bold"> Catégories de cookies utilisées sur le site </h1>
+                    <p> <b> Cookies nécessaires au fonctionnement du site : </b> ils assurent les fonctionnalités essentielles telles que la connexion à votre compte ou l’accès à certaines sections sécurisées. </p>
+                    <p> <b> Cookies de performance et d’analyse : </b> ils nous permettent de recueillir des informations anonymes sur la manière dont le site est utilisé, afin d’en optimiser le contenu et la navigation. </p>
+                    <p> <b> Cookies de personnalisation: </b> ils facilitent votre expérience en enregistrant vos préférences, comme la langue d’affichage ou certaines options utilisateur. </p>
+
+                    <h1 className="text-xl font-bold"> Choix et gestion des cookies </h1>
+                    <p> Lors de votre première visite sur notre site, un bandeau d’information vous permet de paramétrer vos préférences en matière de cookies. Vous avez la possibilité d’accepter tous les cookies, de les refuser intégralement ou de personnaliser votre choix. À tout moment, vous pouvez modifier ces préférences via le lien dédié ou en configurant directement les options de votre navigateur. </p>
+
+                    <h1 className="text-xl font-bold">  Durée de conservation des cookies</h1>
+                    <p> Les cookies que nous utilisons sont conservés pendant une durée maximale de 13 mois, conformément aux recommandations de la CNIL. Passé ce délai, un nouveau consentement vous sera demandé. 
+                    Pour en savoir plus sur l’utilisation que nous faisons de vos données personnelles, nous vous invitons à consulter notre  <b> Politique de protection des données. </b>
+                    </p>
+
+                    
+                </div>
+
+                
 
             </main>
         </>
