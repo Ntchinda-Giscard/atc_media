3a1843e2530cd1aae05206bf03cc9f4f74d45f94
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-04-19 09:22:37 +0100

    div download

--- app/settings/components/account-protection.tsx
+++ app/settings/components/account-protection.tsx
@@ -1,26 +1,23 @@
 import { Button } from "@mantine/core";
 import AccountTable from "./account-table";
+import { DownloadIcon } from "lucide-react"
 
 function AccountProtection() {
     return ( 
         <>
             <div className="flex flex-col gap-4">
                 <p className="text-2xl font-semilight my-5"> Protection des données </p>
-                <Button 
-                    variant="outline" 
-                    color="#EE0202" 
-                    radius={"md"} 
-                    px={10} 
-                    style={{width: 'fit-content'}}
-                >
-                    <div className="flex flex-row items-center"> 
+                <div className="border rounded-xl py-1 px-4 border-red-600 w-fit">
+                    <div className="flex flex-row gap-5 items-center"> 
                         <span> 📥 </span>
                         <div className="flex flex-col justify-start">
                             <p className="text-sm font-semilight"> Télécharger </p>
                             <p className="text-sm text-gray-500"> Télécharger mes données </p>
                         </div>
+                        <DownloadIcon color="#EE0202" />
                     </div>
-                </Button>
+                </div>
+                
                 <p className="text-xl font-semilight my-5"> Historique des connexions </p>
                 <AccountTable />
                 <div className="flex flex-col text-start">
