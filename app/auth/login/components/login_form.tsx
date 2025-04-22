138358e442daff7e25d0095c98bea03258916b85
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-04-22 14:27:05 +0100

    login push to new link

--- app/auth/login/components/login_form.tsx
+++ app/auth/login/components/login_form.tsx
@@ -63,9 +63,9 @@ export function ProfileForm() {
     });
     console.log('Login success:', response.data);
     const user_data = response.data
-    const targetWindow = window.open('http://localhost:3001', '_blank')
+    const targetWindow = window.open('http://ec2-54-147-13-74.compute-1.amazonaws.com', '_blank')
     setTimeout(() =>{
-      targetWindow?.postMessage({ user_data }, 'http://localhos:3001');
+      targetWindow?.postMessage({ user_data }, 'http://ec2-54-147-13-74.compute-1.amazonaws.com');
     }, 1000)
     setErrMessage(null)
     setIsloading(false)
