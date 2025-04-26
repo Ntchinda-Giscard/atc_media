1b23dddc0d7150151c7820596f354e5d88179af6
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-04-26 13:34:01 +0100

    chore: Add loading state and error handling to account deletion

--- app/dashboard/my-account/components/account-protection.tsx
+++ app/dashboard/my-account/components/account-protection.tsx
@@ -5,16 +5,28 @@ import useStore from '@/stores/store';
 //@ts-ignore
 import Cookies from 'js-cookie'
 import {useRouter} from 'next/navigation';
+import { useState } from "react";
+import { error_notification } from "../../utils/notification-center";
 
 function AccountProtection() {
     const router  = useRouter()
     const deleteUser  = useStore(state => state.deleteUser)
     const token = Cookies.get('auth_token');
+    const [isLoading, setIsLoading] = useState(false)
     const handleDelete = async () =>{
-        await deleteUser(token);
-        Cookies.remove('auth_token', { path: '/' })
-        localStorage.removeItem('user');
-        router.push("/auth/login")
+        try{
+            setIsLoading(true)
+            await deleteUser(token);
+            Cookies.remove('auth_token', { path: '/' })
+            localStorage.removeItem('user');
+            setIsLoading(false)
+            router.push("/auth/login")
+        }catch(error){
+            //@ts-ignore
+            error_notification("Echeque de suppression de compte", error?.response?.data?.reason)
+            setIsLoading(false)
+        }
+        
 
     }
     
